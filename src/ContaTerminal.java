a1e89683723939c41c6b65c6a87ade50eb3a5b98
Rayan757575 <rayancatapretastl@gmail.com>
2025-03-17 17:01:49 -0300

    finalizando o desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -12,6 +12,6 @@ public class ContaTerminal{
         System.out.println("Por favor, digite o saldo na conta:");
         double saldo = s.nextDouble();
         System.out.println("\nOlá "+ nomeUsuario+", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numeroAgencia + " e seu saldo " + saldo + " já está disponível para saque.");
-        
+        s.close();
     }
 }
